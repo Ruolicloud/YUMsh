6b76b277f948c52a87cbfd9dbd39a1245d564a16
Ruoli <46256684+Ruolicloud@users.noreply.github.com>
2019-11-16 20:52:57 +0800

    Update yum.sh

--- yum.sh
+++ yum.sh
@@ -11,54 +11,42 @@ echo "It's finish"
 echo "If you want to run again"
 echo "You can enter:yumsh"
 }
-function First(){
 clear
 uptime=`curl -sSL https://raw.githubusercontent.com/Ruolicloud/sh/master/yumupdate.txt`
 system=`rpm -q centos-release|cut -d- -f3`
-case $system in
-      6)
-      exprot ustcurl="https://lug.ustc.edu.cn/wiki/_export/code/mirrors/help/centos?codeblock=2"
-      ;;
-      7)
-      exprot ustcurl="https://lug.ustc.edu.cn/wiki/_export/code/mirrors/help/centos?codeblock=3"
-      ;;
-esac
 #备份原YUM
 mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.bak
 echo "Shell update on ${uptime}"
-echo "Which do you want to get?"
-echo "1.Ali YUM"
-echo "2.163 YUM"
-echo "3.Itzmx YUM"
-echo "4.USTC YUM"
-echo "5.Reduction YUM"
-echo "or enter others to exit"
-read -p "Please choose:" yfrom
+echo "Written by Ruoli"
+yfrom=$1
 case $yfrom in
-     1)
+     -a)
       wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-${system}.repo
+      Finish
       ;;
-     2)
+     -w)
       wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.163.com/.help/CentOS${system}-Base-163.repo
+      Finish
       ;;
-     3)
+     -i)
       wget -O /etc/yum.repos.d/CentOS-Base.repo http://github.itzmx.com/1265578519/mirrors/master/CentOS/CentOS${system}-Base-itzmx.repo
+      Finish
       ;;
-     4)
-      wget -O /etc/yum.repos.d/CentOS-Base.repo ${ustcurl}
-      ;;
-     5)
+     -r)
       rm -rf /etc/yum.repos.d/CentOS-Base.repo /bin/yumsh-log
-      mv /etc/yum.repos.d/CentOS-Base.repo.bak /etc/yum.repos.d/CentOS-Base.repo
+      mv /etc/yumrepos.d/CentOS-Base.repo.bak /etc/yum.repos.d/CentOS-Base.repo
+      Finish
       ;;
      *)
       echo "None"
       mv /etc/yum.repos.d/CentOS-Base.repo.bak /etc/yum.repos.d/CentOS-Base.repo
+      echo "$0:usage: [-a]|[-w]|[-i]|[-r]"
+      echo "[-a] --- Aliyun YUM"
+      echo "[-w] --- Wangyi-163 YUM"
+      echo "[-i] --- Itzmx YUM"
+      echo "[-r] --- Return YUM"
       echo "If you want to run again"
-      echo "You can enter:yumsh"
+      echo "You can enter:yumsh [-a/w/i/r/h]"
       exit
       ;;
 esac
-Finish
-}
-First
