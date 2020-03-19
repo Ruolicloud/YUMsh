f16334619485576acd324d2e25ce5124ea054cdb
Ruoli <46256684+Ruolicloud@users.noreply.github.com>
2020-03-19 08:51:33 +0800

    Update yum.sh

--- yum.sh
+++ yum.sh
@@ -1,52 +1,54 @@
 #!/bin/bash
-#快捷运行 + 自动更新脚本
-wget https://raw.githubusercontent.com/Ruolicloud/sh/master/yum.sh -O /bin/yumsh
-chmod -R 777 /bin/yumsh
-rm -rf $0
+Downurl='https://raw.githubusercontent.com/Ruolicloud/YUMsh/sh'
+uptime=`curl -sSL https://raw.githubusercontent.com/Ruolicloud/sh/master/yumupdate.txt`
+system=`rpm -q centos-release|cut -d- -f3`
 function Finish(){
 yum clean all
 yum makecache
 clear
-echo "It's finish"
+echo "Finish"
 echo "If you want to run again"
 echo "You can enter:yumsh"
 }
-clear
-uptime=`curl -sSL https://raw.githubusercontent.com/Ruolicloud/sh/master/yumupdate.txt`
-system=`rpm -q centos-release|cut -d- -f3`
+#快捷启动
+rm -rf $0
+wget $Downurl/yum.sh -O /usr/bin/yumsh
+chmod -R 777 /usr/bin/yumsh
 #备份原YUM
 mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.bak
+clear
 echo "Shell update on ${uptime}"
 echo "Written by Ruoli"
-yfrom=$1
+echo "1 --- Aliyun YUM"
+echo "2 --- Wangyi-163 YUM"
+echo "3 --- Itzmx YUM"
+echo "4 --- Return YUM"
+echo "If you want to run again"
+echo "You can enter:yumsh"
+read -p "Please choose:" yfrom
 case $yfrom in
-     -a)
+     1)
       wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-${system}.repo
       Finish
       ;;
-     -w)
+     2)
       wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.163.com/.help/CentOS${system}-Base-163.repo
       Finish
       ;;
-     -i)
+     3)
       wget -O /etc/yum.repos.d/CentOS-Base.repo http://github.itzmx.com/1265578519/mirrors/master/CentOS/CentOS${system}-Base-itzmx.repo
       Finish
       ;;
-     -r)
-      rm -rf /etc/yum.repos.d/CentOS-Base.repo /bin/yumsh-log
+     4)
+      rm -rf /etc/yum.repos.d/CentOS-Base.repo
       mv /etc/yumrepos.d/CentOS-Base.repo.bak /etc/yum.repos.d/CentOS-Base.repo
       Finish
       ;;
      *)
       echo "None"
       mv /etc/yum.repos.d/CentOS-Base.repo.bak /etc/yum.repos.d/CentOS-Base.repo
-      echo "$0:usage: [-a]|[-w]|[-i]|[-r]"
-      echo "[-a] --- Aliyun YUM"
-      echo "[-w] --- Wangyi-163 YUM"
-      echo "[-i] --- Itzmx YUM"
-      echo "[-r] --- Return YUM"
       echo "If you want to run again"
-      echo "You can enter:yumsh [-a/w/i/r/h]"
+      echo "You can enter:yumsh"
       exit
       ;;
 esac
